f7f418868b0eac81ead1efb64731421401b6cf72
WemersonPD <wemersondev@gmail.com>
2026-05-03 22:28:22 +0100

    refactor: simplify HomeLayout by removing sidebar open state management and related code

--- src/components/templates/HomeLayout.tsx
+++ src/components/templates/HomeLayout.tsx
@@ -1,23 +1,13 @@
 import type { ReactNode } from 'react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
 
 interface HomeLayoutProps {
   topBar?: ReactNode
   sidebar?: ReactNode
   content?: ReactNode
   pagination?: ReactNode
-  isSidebarOpen?: boolean
-  onCloseSidebar?: () => void
 }
 
-function HomeLayout({
-  topBar,
-  sidebar,
-  content,
-  pagination,
-  isSidebarOpen = false,
-  onCloseSidebar,
-}: HomeLayoutProps) {
+function HomeLayout({ topBar, sidebar, content, pagination }: HomeLayoutProps) {
   return (
     <div className="relative h-screen flex flex-col bg-gray-50 overflow-hidden">
       <a
@@ -35,40 +25,9 @@ function HomeLayout({
       </header>
 
       <div className="flex flex-col lg:flex-row flex-1 min-h-0">
-        {isSidebarOpen && (
-          <div className="lg:hidden fixed inset-0 z-50 flex">
-            <div
-              className="absolute inset-0 bg-black/30"
-              onClick={onCloseSidebar}
-              aria-hidden="true"
-            />
-            <aside
-              aria-label="Filters"
-              className="relative z-10 w-full max-w-sm bg-white overflow-y-auto py-6 px-4"
-            >
-              <div className="flex justify-end mb-2">
-                <button
-                  type="button"
-                  aria-label="Close filters"
-                  onClick={onCloseSidebar}
-                  className="p-1 rounded-lg text-gray-500 hover:bg-gray-100"
-                >
-                  <XMarkIcon className="w-5 h-5" />
-                </button>
-              </div>
-              {sidebar}
-            </aside>
-          </div>
-        )}
+        {sidebar}
 
-        <aside
-          aria-label="Filters"
-          className="hidden lg:block w-64 shrink-0 py-6 px-4 border-r border-gray-200 overflow-y-auto"
-        >
-          {sidebar}
-        </aside>
-
-        <div className="flex flex-col flex-1 min-h-0 lg:px-8">
+        <div className="flex flex-col flex-1 min-h-0 sm:px-8">
           <main
             id="main-content"
             className="flex-1 px-4 sm:px-0 min-h-0 overflow-hidden py-6 flex flex-col"
